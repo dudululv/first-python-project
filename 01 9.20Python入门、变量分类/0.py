a0fdecbcba988967f5391c11ab43f39cb19cb292
dudululv <dudululv@gmail.com>
2025-10-11 00:19:26 +0800

    鑷姩鎻愪氦: 2025/10/11 周六  0:19:26.17

--- 01 9.20Python入门、变量分类/0.py
+++ 01 9.20Python入门、变量分类/0.py
@@ -1,6 +1,9 @@
-'''这是一个多行注释，其实只是一个字符串，
-
-但没有人用它'''
+'''
+这是一个多行注释
+其实只是一个字符串
+但没有人用它
+Python 没有真正的多行注释语法，只有单行注释 #
+'''
 
 print('\'' '1''')
 print('''hello world''')
@@ -9,6 +12,19 @@ a="123456789"
 for i in range(8,1,1):
     print(a[i])
 
+print('='*20)
 x=11
 y=11
 print(id(x),id(y))
+m=12.1
+n=12.1
+print(id(m),id(n),m is n)
+a=[1,2]
+b=[1,2]
+print(a is b)
+print('='*20)
+
+result = print("hello")  # print() 函数返回 None
+print(result)  # 输出: None
+
+input(None)
